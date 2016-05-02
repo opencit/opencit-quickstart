f8ee57df6dfe703b55d635e1adf619bb46521007
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2016-05-02 15:25:48 -0700

    updated isFileAvailable to getAvailableVariants

--- features/cit-quickstart-wizard/src/main/java/com/intel/mtwilson/deployment/jaxrs/io/SoftwarePackageDocument.java
+++ features/cit-quickstart-wizard/src/main/java/com/intel/mtwilson/deployment/jaxrs/io/SoftwarePackageDocument.java
@@ -7,6 +7,7 @@ package com.intel.mtwilson.deployment.jaxrs.io;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.intel.mtwilson.jaxrs2.Document;
+import java.util.Set;
 
 /**
  * Faults may be added by the server; a client should not send a document with
@@ -17,25 +18,23 @@ import com.intel.mtwilson.jaxrs2.Document;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JacksonXmlRootElement(localName = "package")
 public class SoftwarePackageDocument extends Document {
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SoftwarePackageDocument.class);
-
     private String packageName;
-    private Boolean available;
+    private Set<String> availableVariants;
 
     public void setName(String packageName) {
         this.packageName = packageName;
     }
 
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-    
     public String getName() {
         return packageName;
     }
 
-    public Boolean getAvailable() {
-        return available;
+    public void setAvailableVariants(Set<String> availableVariants) {
+        this.availableVariants = availableVariants;
+    }
+
+    public Set<String> getAvailableVariants() {
+        return availableVariants;
     }
     
 }
